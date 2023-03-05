fa30fe475773c576a14dd29bf7c7a57a10aad9cd
Aaron <aaronrebollar@gamil.com>
2023-03-05 13:49:02 -0600

    Add+ scanf of float

---
+++ basic_examples/keyboard_input/input_float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main(){
+    float testInput; //local variable and float type that will be assignerd the input
+    printf("Enter an number: ");
+    scanf("%f", &testInput);
+    printf("Number= %f", testInput, "\n");
+    system("pause");
+}
